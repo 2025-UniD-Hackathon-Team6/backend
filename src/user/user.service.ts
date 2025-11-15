610ec45c51541da6ffe3e6a9527b8a73dbb6f1b0
hj689git <hjbanf89@gmail.com>
2025-11-15 16:56:33 +0900

    feat(be): connect prisma

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
+import { PrismaService } from '@libs/prisma';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async Test() {
+    return await this.prisma.job.findMany();
+  }
+}
